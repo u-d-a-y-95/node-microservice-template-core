eb2c0e59c0b79cbaa9f76e40195807e794170ba8
uDay <uday.dev.bd@gmail.com>
2025-06-20 15:38:48 +0600

    chore: config gloabl error handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
+import { HttpError } from "./error";
+import { logger } from "./logger";
 
 export const app = express();
+
+app.use(express.json());
+
+app.use(((error, _req, res, _next) => {
+  logger.error(error);
+
+  if (error instanceof HttpError) {
+    res.status(error.status).json({
+      status: error.status,
+      message: error.message,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: "Internal Server Error",
+  });
+}) satisfies ErrorRequestHandler);
